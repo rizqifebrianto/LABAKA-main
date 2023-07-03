123ca45aeafb770edf7b00cc9ed8ad503f659615
syafiraaliffinda <95162649+syafiraaliffinda@users.noreply.github.com>
2023-07-03 15:32:19 +0700

    Update v_about.php

--- application/views/depan/v_about.php
+++ application/views/depan/v_about.php
@@ -85,7 +85,7 @@
         <div class="row">
             <div class="col-md-7">
                 <h2>Tentang Laboratorium Biologi</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p>Laboratorium Biologi SMA S AL-KAUTSAR merupakan salah satu sarana yang berfungsi sebagai alat pendukung aktivitas siswa dalam proses belajar mengajar, melakukan penelitian dan pengamatan, serta dapat meningkatkan keterampilan siswa.  </p>
                 </div>
                 <div class="col-md-5">
                     <img src="<?php echo base_url().''?>" class="img-fluid" alt="#">
